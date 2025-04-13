e84ff7e1aa6f33904f93adab5f7bd9b219e206dc
harapeco815 <sufiakhalid5125@gmail.com>
2025-04-13 21:54:46 +0500

    .

--- DBMS/Backend/index.js
+++ DBMS/Backend/index.js
@@ -1,13 +1,15 @@
 import express from "express"
 import cors from "cors"
+import eventHistoryRoute from "./route/showEvents.js";
+
 
 const app = express();
+const port = 8800;
 
 app.use(express.json())
 app.use(cors())
 
-const port = 8800;
-
+app.use("/", eventHistoryRoute)
 
 app.listen(port, () => {
     console.log("Connected!");
